6847cee8f4ea1247d67a1453404910a46112cae9
Pete Bacon Darwin <pete@bacondarwin.com>
2021-05-22 18:27:19 +0100

    add $localize ICU handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import IntlMessageFormat from 'intl-messageformat';
+
+function formatIcu(icu: string) {
+  const msg = new IntlMessageFormat(icu, $localize.locale);
+  return (context: any) => msg.format(context);
+}
+
+const someICUMessage = formatIcu($localize`{count, plural, =0 {none} =1 {one} other {more}}`);
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div i18n>{{count}} - {count, plural, =0 {none} =1 {one} other {more}}</div>
+
+    <button (click)="count = count + 1">+1</button>
+    <button (click)="count = count - 1">-1</button>
+
+    <pre>{{message}}</pre>
+  `,
 })
 export class AppComponent {
-  title = 'localize-icu-test';
+  count = 0;
+
+
+  get message() {
+    return someICUMessage(this);
+  }
 }
